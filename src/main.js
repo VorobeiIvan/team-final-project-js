c36a270e5fd1e69f1225ec2c65a82b4d50acc98d
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2023-12-13 21:17:12 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import 'izitoast/dist/css/iziToast.min.css';
 import './js/filters.js';
-import './js/quote.js';
+import './js/quote/template.js';
 import './js/components/';
 import './js/footer.js';
 import './js/scroll-up.js';
 import './js/header.js';
 import './js/burger-menu.js';
-
