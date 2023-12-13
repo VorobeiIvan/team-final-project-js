b7392f2e7faa89e2bbc941b37d1493ebee83d717
Ivan <0675361905vi@gmail.com>
2023-12-13 17:45:37 +0200

    bug fixes

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', handlePageNavigation)
+document.addEventListener('DOMContentLoaded', handlePageNavigation);
 
 function handlePageNavigation() {
-    const currentPage = location.href;
-    const links = document.querySelectorAll('.header-nav-link');
-    links.forEach(function(link) {
-        if (link.href === currentPage) {
-            link.classList.add('active-page');
-        } else {
-            link.classList.remove('active-page');
-        }
-    });
-};
+  const currentPage = location.pathname;
+  const links = document.querySelectorAll('.header-nav-link');
+  links.forEach(function(link) {
+    const linkPath = new URL(link.href).pathname;
+    if (linkPath === currentPage || (currentPage === '/' && linkPath === '/index.html')) {
+      link.classList.add('active-page');
+    } else {
+      link.classList.remove('active-page');
+    }
+  });
+}
