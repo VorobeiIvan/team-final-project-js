c404b24ceeb14ec0f02b2e2e6ae5d2a755be2fe9
Dmytro-Chernomord <66522870+Dmytro-Chernomord@users.noreply.github.com>
2023-12-14 08:34:48 +0000

    Deploying to gh-pages from @ VorobeiIvan/team-final-project-js@00b2f85823c9f6c970ad5ad8426f5adff59c35bc 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,45 @@
-import"./assets/burger-menu-5139967a.js";/* empty css                      */import{a as d,P as f,i as c}from"./assets/vendor-61000f44.js";d.defaults.baseURL="https://your-energy.b.goit.study/api";const v="filters",E="subscription",w=async({page:e=1,perPage:s=12,filter:o="Muscles"}={})=>await d.get(`/${v}?filter=${o}&page=${e}&limit=${s}`).then(t=>t.data),q=async()=>{try{const{data:e}=await d.get("/quote");return e}catch{console.log('Error request "/quote"')}},b=async e=>await d.post(`/${E}`,{email:e}).then(s=>s.data);function g(e){return e.toString().padStart(2,"0")}function C(e){return[e.getFullYear(),g(e.getMonth()+1),g(e.getDate())].join("-")}const T=({quote:e,author:s})=>`
+import"./assets/burger-menu-5139967a.js";/* empty css                      */import{a as m,P as w,i as d}from"./assets/vendor-61000f44.js";const r={galleryRef:document.querySelector(".gallery"),closeBtn:document.querySelector(".modal_close_btn"),backdrop:document.querySelector(".backdrop"),modalContainer:document.querySelector(".modal-container"),cardRef:document.querySelector(".card")};console.log("refs.modalContainer: ",r.modalContainer);r.cardRef.addEventListener("click",L);async function x(e){return await(await fetch("https://your-energy.b.goit.study/api/exercises/64f389465ae26083f39b17a2")).json()}function L(e){x().then(t=>{C(t)}),r.backdrop.classList.remove("is-hidden"),document.body.classList.add("no-scroll"),r.backdrop.classList.add("scroll"),q()}function q(){document.addEventListener("click",e=>{e.target===r.backdrop&&(r.backdrop.classList.add("is-hidden"),document.body.classList.remove("no-scroll"),r.backdrop.classList.remove("scroll"))}),document.addEventListener("keydown",e=>{e.key==="Escape"&&(r.backdrop.classList.add("is-hidden"),document.body.classList.remove("no-scroll"),r.backdrop.classList.remove("scroll"))}),r.closeBtn.addEventListener("click",()=>{r.backdrop.classList.add("is-hidden"),document.body.classList.remove("no-scroll"),r.backdrop.classList.remove("scroll")})}function C(e){const t=e.gifUrl,o=e.name,s=Number(e.rating).toFixed(1),a=e.target,i=e.bodyPart,n=e.equipment,c=e.popularity,u=e.burnedCalories,E=e.description;return r.modalContainer.innerHTML=`
+      <div class="modal-img">
+        <img src="${t}" alt="${o}" class="image" />
+      </div>
+      <div class="modal-rest">
+        <h3 class="modal-title">${o}</h3>
+        <div class="rating-element">
+            <p class="rating-count" id="vote">${s}</p>
+            <p class="info-item">******</p>
+        </div>
+        <div class="modal-info">
+          <div class="modal-table">
+            <div class="info-element">
+              <p class="info-item">Target</p>
+              <p class="count">${a}</p>
+            </div>
+            <div class="info-element">
+              <p class="info-item">Body part</p>
+              <p class="count">${i}</p>
+            </div>
+            <div class="info-element">
+              <p class="info-item">Equipment</p>
+              <p class="count">${n}</p>
+            </div>
+            <div class="info-element">
+              <p class="info-item">Popular</p>
+              <p class="count">${c}</p>
+            </div>
+            <div class="info-element">
+              <p class="info-item">Burned Calories</p>
+              <p class="count">${u}</p>
+            </div>
+          </div>
+          <p class="about-info">${E}</p>
+        </div>
+        <div class="buttons">
+          <button class="button add-to-favorites" type="button">
+            Add to favorites
+          </button>
+          <button class="button give-a-rating" type="button">Give a rating</button>
+        </div>
+      </div>`}m.defaults.baseURL="https://your-energy.b.goit.study/api";const k="filters",$="exercises",S="subscription",B=async({page:e=1,perPage:t=12,filter:o="Muscles"}={})=>await m.get(`/${k}?filter=${o}&page=${e}&limit=${t}`).then(s=>s.data),T=async({page:e=1,perPage:t=12,filter:o={}}={})=>await m.get(`/${$}?${new URLSearchParams(o).toString()}&page=${e}&limit=${t}`).then(s=>s.data),M=async()=>{try{const{data:e}=await m.get("/quote");return e}catch{console.log('Error request "/quote"')}},I=async e=>await m.post(`/${S}`,{email:e}).then(t=>t.data);function v(e){return e.toString().padStart(2,"0")}function R(e){return[e.getFullYear(),v(e.getMonth()+1),v(e.getDate())].join("-")}const N=({quote:e,author:t})=>`
   <div class="quote-container">
     <div class="quote-wrap">
       <div class="quote-wrapper">
@@ -15,7 +56,7 @@ import"./assets/burger-menu-5139967a.js";/* empty css                      */imp
             </svg>
           </div>
           <p class="quote-text">${e}</p>
-          <p class="quote-author">${s}</p>
+          <p class="quote-author">${t}</p>
         </div>
       </div>
       <img class="quote-img" src="../../../images/exercise-mobile@2x.jpg" alt="exercise" />
@@ -35,5 +76,56 @@ import"./assets/burger-menu-5139967a.js";/* empty css                      */imp
       </div>
     </div>
   </div>
-`;const p="quoteData",h=new Date,M=async()=>{let e=JSON.parse(localStorage.getItem(p));return(!e||e.date!==h)&&(e=await q(),localStorage.setItem(p,JSON.stringify({date:C(h),...e}))),e},B=async()=>{const e=document.getElementById("js-quote"),s=await M();e.insertAdjacentHTML("beforeend",T(s))},I=(e,s,o)=>{const t=document.getElementById("tui-pagination-container"),i=s<5?s:5,n={totalItems:e*s,itemsPerPage:e,visiblePages:i,centerAlign:!0,page:o},a=new f(t,n);return i<=1?t.style.display="none":t.style.display="block",a},l={divQuote:document.getElementById("result-quote"),getQuoteButton:document.getElementById("quote"),divCategories:document.getElementById("categories"),filtersRef:document.getElementById("filters"),divExercises:document.getElementById("exercises"),footerForm:document.querySelectorAll(".footer-form")};async function m(e,s){const o=document.getElementById("categories-loader"),t=document.getElementById("categories-wrapper");o.style.display="block",t.style.display="none",l.divCategories.innerHTML="";try{B();const i=await w({page:s,perPage:12,filter:e});I(12,i.totalPages,s).on("afterMove",({page:a})=>{m(e,a)});const n=i.results.map(a=>{const r=L(a);return r.addEventListener("click",function(){console.log("Li element clicked!")}),r});l.divCategories.append(...n)}catch(i){if(i.response&&i.response.status===409){const a={title:"Error",message:i.response.data.message,position:"topRight",color:"red"};return c.show(a)}const n={title:"Error",message:"Oops, something went wrong, try again later",position:"topRight",color:"red"};return c.show(n)}finally{setTimeout(()=>{o.style.display="none",t.style.display="flex"},500)}}function L({name:e,filter:s,imgURL:o}){const t=document.createElement("li");t.className="category-item";const i=document.createElement("img");i.className="category-item-img",i.src=o,i.alt="Category",i.loading="lazy";const n=document.createElement("div");n.className="category-item-bg";const a=document.createElement("p");a.className="category-item-title",a.textContent=k(e);const r=document.createElement("p");return r.className="category-item-subtitle",r.textContent=s,n.appendChild(a),n.appendChild(r),t.appendChild(i),t.appendChild(n),t}function k(e){return e.charAt(0).toUpperCase()+e.slice(1)}const y=["Muscles","Body parts","Equipment"];m(y[0],1);const R=S(y);l.filtersRef.append(...R);function S(e){return e.map(s=>{const o=document.createElement("li");o.setAttribute("id",s),s==="Muscles"&&o.classList.add("filter-selected");const t=document.createElement("button");return t.innerHTML=s,t.addEventListener("click",()=>{x(s,o)}),o.appendChild(t),o})}function x(e,s){const o=document.getElementsByClassName("filter-selected");o[0]&&(o[0].classList.remove("filter-selected"),s.classList.add("filter-selected"),l.divCategories.innerHTML="",m(e,1))}const A=e=>/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e),D=e=>{e.preventDefault();const{email:s}=e.currentTarget.elements;if(!A(s.value.trim())){const t={title:"Error",message:"The email must be in format test@gmail.com",position:"topRight",color:"red"};return c.show(t)}const o=s.value.trim();b(o).then(t=>{if(t.message){const i={title:"Success",message:t.message,position:"topRight",color:"green"};return c.show(i)}}).catch(t=>{if(t.response&&t.response.status===409){const n={title:"Error",message:t.response.data.message,position:"topRight",color:"red"};return c.show(n)}const i={title:"Error",message:"Oops, something went wrong, try again later",position:"topRight",color:"red"};return c.show(i)}).finally(()=>l.footerForm.forEach(t=>t.reset()))};l.footerForm.forEach(e=>e.addEventListener("submit",D));const u=document.getElementById("myBtn");window.onscroll=function(){F()};function F(){document.body.scrollTop>0||document.documentElement.scrollTop>0?u.style.display="block":u.style.display="none"}u.addEventListener("click",()=>{document.body.scrollTop=0,document.documentElement.scrollTop=0});
+`;const y="quoteData",h=new Date,A=async()=>{let e=JSON.parse(localStorage.getItem(y));return(!e||e.date!==h)&&(e=await M(),localStorage.setItem(y,JSON.stringify({date:R(h),...e}))),e},D=async()=>{const e=document.getElementById("js-quote"),t=await A();e.insertAdjacentHTML("beforeend",N(t))},P=(e,t,o)=>{const s=document.getElementById("tui-pagination-container"),a=t<5?t:5,i={totalItems:e*t,itemsPerPage:e,visiblePages:a,centerAlign:!0,page:o},n=new w(s,i);return a<=1?s.style.display="none":s.style.display="block",n},l={divQuote:document.getElementById("result-quote"),getQuoteButton:document.getElementById("quote"),divCategories:document.getElementById("categories"),filtersRef:document.getElementById("filters"),divExercises:document.getElementById("exercises"),footerForm:document.querySelectorAll(".footer-form"),exSearch:document.getElementById("exercises-search")};async function f(e,t){const o=document.getElementById("categories-loader"),s=document.getElementById("categories-wrapper");o.style.display="block",s.style.display="none",l.divCategories.innerHTML="";try{D();const a=await B({page:t,perPage:12,filter:e});P(12,a.totalPages,t).on("afterMove",({page:n})=>{f(e,n)});const i=a.results.map(n=>{const c=F(n);return c.addEventListener("click",()=>{O(c)}),c});l.divCategories.append(...i)}catch(a){if(a.response&&a.response.status===409){const n={title:"Error",message:a.response.data.message,position:"topRight",color:"red"};return d.show(n)}const i={title:"Error",message:"Oops, something went wrong, try again later",position:"topRight",color:"red"};return d.show(i)}finally{setTimeout(()=>{o.style.display="none",s.style.display="flex"},500)}}function F({name:e,filter:t,imgURL:o}){const s=document.createElement("li");s.className="category-item";const a=t==="Body parts"?"bodypart":t;s.setAttribute("data-"+a,e);const i=document.createElement("img");i.className="category-item-img",i.src=o,i.alt="Category",i.loading="lazy";const n=document.createElement("div");n.className="category-item-bg";const c=document.createElement("p");c.className="category-item-title",c.textContent=j(e);const u=document.createElement("p");return u.className="category-item-subtitle",u.textContent=t,n.appendChild(c),n.appendChild(u),s.appendChild(i),s.appendChild(n),s}function j(e){return e.charAt(0).toUpperCase()+e.slice(1)}function O(e){const t=document.getElementsByClassName("card-selected");t[0]&&t[0].classList.remove("card-selected"),e.classList.add("card-selected"),console.log("Li element clicked!")}const b=["Muscles","Body parts","Equipment"];f(b[0],1);const H=Q(b);l.filtersRef.append(...H);function Q(e){return e.map(t=>{const o=document.createElement("li");o.setAttribute("id",t),t==="Muscles"&&o.classList.add("filter-selected");const s=document.createElement("button");return s.innerHTML=t,s.addEventListener("click",()=>{U(t,o)}),o.appendChild(s),o})}function U(e,t){const o=document.getElementsByClassName("filter-selected");o[0]&&(o[0].classList.remove("filter-selected"),t.classList.add("filter-selected"),l.divCategories.innerHTML="",l.exSearch.value="",f(e,1))}const z=e=>/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e),W=e=>{e.preventDefault();const{email:t}=e.currentTarget.elements;if(!z(t.value.trim())){const s={title:"Error",message:"The email must be in format test@gmail.com",position:"topRight",color:"red"};return d.show(s)}const o=t.value.trim();I(o).then(s=>{if(s.message){const a={title:"Success",message:s.message,position:"topRight",color:"green"};return d.show(a)}}).catch(s=>{if(s.response&&s.response.status===409){const i={title:"Error",message:s.response.data.message,position:"topRight",color:"red"};return d.show(i)}const a={title:"Error",message:"Oops, something went wrong, try again later",position:"topRight",color:"red"};return d.show(a)}).finally(()=>l.footerForm.forEach(s=>s.reset()))};l.footerForm.forEach(e=>e.addEventListener("submit",W));const g=document.getElementById("myBtn");window.onscroll=function(){_()};function _(){document.body.scrollTop>0||document.documentElement.scrollTop>0?g.style.display="block":g.style.display="none"}g.addEventListener("click",()=>{document.body.scrollTop=0,document.documentElement.scrollTop=0});const p="/team-final-project-js/assets/sprite-984a0ff0.svg";document.querySelector(".exercises-search-wrapper input").addEventListener("keydown",G);async function G(e){const t=e.target.value;if(e.keyCode===13||e.code==="Enter"){const o=document.getElementsByClassName("card-selected")[0];if(!o){console.log("not active card!!!");return}const s=o.dataset,a={keyword:t};for(const i in s)a[i]=s[i];console.log(a);try{const i=await T({page:1,perPage:10,filter:a}),n=J(i.results);l.divCategories.innerHTML=n,console.log(n)}catch{console.log("ooops!!!")}}}function J(e){return e.map(({name:t,target:o,rating:s,burnedCalories:a,time:i,_id:n,bodyPart:c})=>`
+               <li class="exercise-item">
+          <div class="exercise-item-wrapper">
+            <div class="exercise-item-firth-wrapper">
+              <p class="exercise-item-workout">WORKOUT</p>
+              <p class="exercise-item-rating">${s}</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 100 130"
+                class="exercise-item-star"
+              >
+                <use href="${p}#icon-Star-2"></use>
+              </svg>
+              <button type="button" class="exercise-item-button" id=${n}>
+                Start&nbsp;&nbsp;
+                <svg width="16" height="16">
+                  <use href="${p}#arrow"></use>
+                </svg>
+              </button>
+            </div>
+            <div class="exercise-item-second-wrapper">
+              <div class="exercise-item-run-box">
+                <svg class="exercise-item-run" width="16" height="16">
+                  <use href="${p}#Group"></use>
+                </svg>
+              </div>
+              <h3 class="exercise-item-subtitle">${t}</h3>
+            </div>
+            <ul class="exercise-item-list">
+              <li class="exercise-item-list-information">
+                <p class="information-text">
+                  Burned calories:<span class="information-text-span"
+                    >${a} / ${i} min</span
+                  >
+                </p>
+              </li>
+              <li class="exercise-item-list-information">
+                <p class="information-text">
+                  Body part:<span class="information-text-span">${c}</span>
+                </p>
+              </li>
+              <li class="exercise-item-list-information">
+                <p class="information-text">
+                  Target:<span class="information-text-span">${o}</span>
+                </p>
+              </li>
+            </ul>
+          </div>
+        </li>
+         `).join("")}
 //# sourceMappingURL=commonHelpers2.js.map
