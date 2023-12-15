7bfd72e8cb31dcd74e7ec00c9496996b283a381b
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2023-12-15 03:53:45 +0000

    add functionality, clean code

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -1,50 +1,49 @@
-import svgSprite from "../img/sprite.svg";
+import svgSprite from '../img/sprite.svg';
 import { fetchExercises } from './api.js';
 import refs from './refs.js';
 
 document
-    .querySelector('.exercises-search-wrapper input')
-    .addEventListener('keydown', toSearch);
+  .querySelector('.exercises-search-wrapper input')
+  .addEventListener('keydown', toSearch);
 
 function toSearch(event) {
   const keyword = event.target.value;
   if (event.keyCode === 13 || event.code === 'Enter') {
     renderExercises(keyword);
-  }          
- }
-    
-export async function renderExercises(keyword='') {
-  
-    const curFilter = await JSON.parse(sessionStorage.getItem("category"));
-    if (!curFilter) {
-      console.log('not active category!!!')
-      return;
-    } 
-    if (keyword) {
+  }
+}
+
+export async function renderExercises(keyword = '') {
+  const curFilter = await JSON.parse(sessionStorage.getItem('category'));
+  if (!curFilter) {
+    console.log('not active category!!!');
+    return;
+  }
+  if (keyword) {
     curFilter.keyword = keyword;
-    };
- 
+  }
+
   try {
     const data = await fetchExercises({
       page: 1,
       perPage: 10,
       filter: curFilter,
     });
-        
-    const exercisesToRender = createExercise(data.results);
 
+    const exercisesToRender = createExercise(data.results);
     refs.divCategories.innerHTML = exercisesToRender;
     refs.divCategories.classList.add('exercises-list');
   } catch {
     console.log('ooops!!!');
   }
 }
-               
+
 function createExercise(arr) {
-    return arr
-      .map(
-        ({ name, target, rating, burnedCalories, time, _id, bodyPart }) => `
-               <li class="exercise-item">
+  return arr
+    .map(({ name, target, rating, burnedCalories, time, _id, bodyPart }) => {
+      const itemId = `${_id}item`;
+      return `
+               <li class="exercise-item" id=${itemId}>
           <div class="exercise-item-wrapper">
             <div class="exercise-item-firth-wrapper">
               <p class="exercise-item-workout">WORKOUT</p>
@@ -88,8 +87,7 @@ function createExercise(arr) {
             </ul>
           </div>
         </li>
-         `
-      )
-      .join('');
-  }
-
+         `;
+    })
+    .join('');
+}
--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import 'izitoast/dist/css/iziToast.min.css';
 import './js/filters.js';
 import './js/components/';
 import './js/footer.js';
-import './js/modal_main.js';
+import './js/modal-main.js';
 import './js/scroll-up.js';
 import './js/header.js';
 import './js/burger-menu.js';
 import './js/exercises.js';
 import './js/pagination.js';
-
