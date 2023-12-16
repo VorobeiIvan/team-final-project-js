a398a86b968148f70477d5343c21f9706f2c8f25
Dmytro Chernomord <tschernomord@gmail.com>
2023-12-16 16:43:13 +0100

    styles

--- src/js/modal-main.js
+++ src/js/modal-main.js
@@ -10,11 +10,11 @@ refs.divCategories.addEventListener('click', handleExerciseCardClick);
 let activeItem;
 
 const removeButtonContent = `Remove from favorites
-        <svg class='modal-icon-heart'>
+        <svg class='modal-icon-trash' width='16' height='16'>
           <use href='./images/sprite.svg#trash'></use>
         </svg>`;
 const addButtonContent = `Add to favorites
-        <svg class='modal-icon-heart'>
+        <svg class='modal-icon-heart'  width='16' height='16'>
           <use href='./images/sprite.svg#heart'></use>
         </svg>`;
 
